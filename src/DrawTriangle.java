3222a5b885da2725c1bde438182444090b4db90b
kboss <atsugi.design@gmail.com>
2023-07-07 15:15:58 +0900

    三角形の描画

--- src/DrawTriangle.java
+++ src/DrawTriangle.java
@@ -1,15 +1,71 @@
 public class DrawTriangle {
 
-    public void drawSideTriangle(int height){
-
+    public void drawSideTriangle(int height) {
+        for (int i = 0; i <= height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        //頂点に達したら１まで減少する
+        for (int k = 1; k < height; k++) {
+            for (int l = height; l > k; l--) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
-    public void drawDownTriangle(int height){
 
+    public void drawDownTriangle(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= height * 2 - (i * 2) - 2; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
-    public void drawUpTriangle(int height){
 
+    public void drawUpTriangle(int height) {
+        System.out.println();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // #の数分ループ
+            for (int k = 0; k < i * 2 + 1; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
-    public void drawDiamond(int height){
 
+    public void drawDiamond(int height) {
+// 上向きの三角形と下向きの三角形を合体するイメージ
+        for (int i = 0; i < height; i++) {
+            // 空白で表示位置を調整
+            for (int j = 0; j < height - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // #の数分ループ
+            for (int k = 0; k < i * 2 + 1; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < height - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < height * 2 - (i * 2) - 3; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Triangle {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("三角形の高さを入力してください(1~10の整数)");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         int height = Integer.parseInt(input);
         //範囲を確認
-        if(height > 10 || height <= 0){
+        if (height > 10 || height <= 0) {
             System.out.println("入力が不正です");
         } else {
             System.out.println("「" + height + "」が入力されました");
